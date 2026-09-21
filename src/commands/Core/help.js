4ed12e0693a851a43de38556a0df28fc667a3b2b
Seythure <seythure@gmail.com>
2026-09-21 01:58:09 -0700

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -98,11 +98,6 @@ export async function createInitialHelpMenu(client) {
                 ].join('\n'),
                 inline: false,
             },
-            {
-                name: '\u200B',
-                value: `-# ${botName} is [open source](https://youtu.be/1jCZX8s3bJE?si=NPOYx-vxVE1I5vJK)`,
-                inline: false,
-            },
         ],
     });
 
